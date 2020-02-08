987307865e6c2d546fa29038289f46d48daea810
pradhumn9142 <60519512+pradhumn9142@users.noreply.github.com>
2020-02-08 10:18:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const numbers = document.querySelectorAll(".number")
+//console.log(numbers)
+numbers.forEach((number) => {
+number.addEventListener("click", (event) => {
+inputNumber(event.target.value)
+updateScreen(currentInput)
+})
+//console.log(number)
+})
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+calculatorScreen.value = number
+}
+let prevInput = '0'
+let calculationOperator = ''
+let currentInput = '0'
+
+const inputNumber= (number) => {
+if (currentInput ==='0') {
+currentInput = number
+} else {
+currentInput += number
+}
+}
+const operators = document.querySelectorAll(".operator")
+operators.forEach((operator) => {
+operator.addEventListener("click", (event) => {
+inputOperator(event.target.value)
+})
+})
+const inputOperator = (operator) => {
+prevInput = currentInput
+calculationOperator = operator
+currentInput = '0'
+}
+const calculate = () => {
+let result = 0
+switch(calculationOperator) {
+case '+':
+result = parseInt(prevInput) + parseInt(currentInput)
+break
+case '-':
+result = parseInt(prevInput) - parseInt(currentInput)
+break
+case '*':
+result = parseInt(prevInput) * parseInt(currentInput)
+break
+case '/':
+result = parseInt(prevInput) / parseInt(currentInput)
+break  
+default:
+return
+}
+currentInput = result.toString()
+calculationOperator = ''
+}
+const equalSign = document.querySelector('.equal-sign')
+equalSign.addEventListener( 'click',() => {
+calculate()
+updateScreen(currentInput)
+})
+
+const clearAll = () => {
+prevInput = '0'
+calculationOperator = ''
+currentInput = '0'
+}
+const clearBtn = document.querySelector('.all-clear')
+clearBtn.addEventListener('click', () => {
+clearAll()
+updateScreen(currentInput)
+})
+
+
+
+
